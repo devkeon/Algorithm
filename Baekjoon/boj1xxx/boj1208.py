faf73723b8405b1b14a407a848f9b70dba6e55b3
Keon <lune0410k@naver.com>
2023-10-01 15:01:23 +0900

    meet in the middle(performance opt)

--- Baekjoon/boj1xxx/boj1208.py
+++ Baekjoon/boj1xxx/boj1208.py
@@ -5,46 +5,31 @@ n, s = map(int, read().split())
 seq = [*map(int, read().split())]
 seq.sort()
 mid = n // 2
-left, right = seq[:mid], seq[mid:]
 left_seq = {}
-right_seq = {}
 ans = 0
 
 
 def leftcomb(cur, subsum):
     if cur == mid:
+        if subsum in left_seq.keys():
+            left_seq[subsum] += 1
+        else:
+            left_seq[subsum] = 1
         return
-    newsum = subsum + seq[cur]
-    if newsum in left_seq.keys():
-        left_seq[newsum] += 1
-    else:
-        left_seq[newsum] = 1
     leftcomb(cur + 1, subsum)
-    leftcomb(cur + 1, newsum)
+    leftcomb(cur + 1, subsum + seq[cur])
 
 
 def rightcomb(cur, subsum):
     if cur == n:
+        global ans
+        if s - subsum in left_seq.keys():
+            ans += left_seq[s - subsum]
         return
-    newsum = subsum + seq[cur]
-    if newsum in right_seq.keys():
-        right_seq[newsum] += 1
-    else:
-        right_seq[newsum] = 1
     rightcomb(cur + 1, subsum)
-    rightcomb(cur + 1, newsum)
+    rightcomb(cur + 1, subsum + seq[cur])
 
 
 leftcomb(0, 0)
 rightcomb(mid, 0)
-leftkey = list(left_seq.keys())
-rightkey = right_seq.keys()
-for i in range(len(leftkey)):
-    toRight = s - leftkey[i]
-    if toRight in rightkey:
-        ans += (left_seq[leftkey[i]] * right_seq[toRight])
-if s in leftkey:
-    ans += left_seq[s]
-if s in rightkey:
-    ans += right_seq[s]
-print(ans)
+print(ans if s != 0 else ans - 1)
