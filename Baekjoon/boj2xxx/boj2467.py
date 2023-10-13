cbe74d259c9ba36a5eae0dd0f88ab656f3ebd0b1
Keon <lune0410k@naver.com>
2023-10-13 23:28:15 +0900

    two pointer

--- Baekjoon/boj2xxx/boj2467.py
+++ Baekjoon/boj2xxx/boj2467.py
@@ -4,32 +4,17 @@ read = sys.stdin.readline
 ans, total = [-1, -1], float('inf')
 n = int(read())
 liq = [*map(int, read().split())]
-
-
-def bs(start, end, base):
-    back = [float('inf'), -1]
-    while start <= end:
-        mid = (start + end) // 2
-        summ = base + liq[mid]
-        if abs(summ) < back[0]:
-            back[0] = abs(summ)
-            back[1] = liq[mid]
-        if summ < 0:
-            start = mid + 1
-        elif summ > 0:
-            end = mid - 1
-        else:
-            back[0] = abs(summ)
-            back[1] = liq[mid]
-            break
-    return back
-
-
-for i in range(n - 1):
-    arr = bs(i + 1, n - 1, liq[i])
-    if total > arr[0]:
-        total = arr[0]
-        ans = [liq[i], arr[1]]
+l, r = 0, n - 1
+while l < r:
+    summ = liq[l] + liq[r]
+    if total > abs(summ):
+        total = abs(summ)
+        ans[0], ans[1] = liq[l], liq[r]
+    if summ < 0:
+        l += 1
+    elif summ > 0:
+        r -= 1
+    else:
+        break
 
 print(*ans)
-
