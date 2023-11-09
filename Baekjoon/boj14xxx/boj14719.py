0b3136f4fe002ee888e60cfb1111963e259b827f
Keon <lune0410k@naver.com>
2023-11-09 11:50:42 +0900

    dp simulation

--- Baekjoon/boj14xxx/boj14719.py
+++ Baekjoon/boj14xxx/boj14719.py
@@ -3,14 +3,15 @@ read = sys.stdin.readline
 
 h, w = map(int, read().split())
 blocks = [*map(int, read().split())]
+dp = [-1 for _ in range(w)]
+dp[0] = blocks[0]
 ans = 0
-for i in range(1, w - 1):
-    rmax, lmax = blocks[i], blocks[i]
-    for j in range(i - 1, -1, -1):
-        lmax = max(lmax, blocks[j])
-    for j in range(i + 1, w):
-        rmax = max(rmax, blocks[j])
-    minn = min(rmax, lmax)
-    if minn != -1:
-        ans += (min(rmax, lmax) - blocks[i])
+for i in range(1, w):
+    dp[i] = max(blocks[i], dp[i - 1])
+minn = blocks[-1]
+for i in range(w - 1, -1, -1):
+    minn = max(minn, blocks[i])
+    dp[i] = min(minn, dp[i])
+    if dp[i] > blocks[i]:
+        ans += (dp[i] - blocks[i])
 print(ans)
