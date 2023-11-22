41108060f6fa90040928e7ea738eb5bee5bab33c
Keon <lune0410k@naver.com>
2023-11-22 12:47:30 +0900

    modify 0 count

--- Baekjoon/boj7xxx/boj7579.py
+++ Baekjoon/boj7xxx/boj7579.py
@@ -8,14 +8,14 @@ C = [*map(int, read().split())]
 cost_sum = sum(C)
 dp = [[0 for _ in range(cost_sum + 1)] for k in range(n)]
 ans = 10001
-for i in range(1, cost_sum + 1):
+for i in range(cost_sum + 1):
     if C[0] <= i:
         dp[0][i] = A[0]
         if dp[0][i] >= m:
             ans = min(ans, i)
 
 for i in range(1, n):
-    for c in range(1, cost_sum + 1):
+    for c in range(cost_sum + 1):
         if C[i] > c:
             dp[i][c] = dp[i - 1][c]
         else:
